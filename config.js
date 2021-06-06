7e9babb78bb33591976cce49d90eab0541798df5
Ayan Biswas <ayanbiswas184@gmail.com>
2021-06-06 17:02:18 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+
+module.exports = {
+    prefix: "cov",
+	token: process.env.TOKEN
+};
---
+++ index.js
@@ -0,0 +1,24 @@
+const config = require('./config');
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', message => {
+    let prefix = config.prefix;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		message.channel.send('Pong.');
+	} else if (command === 'beep') {
+		message.channel.send('Boop.');
+	}
+	// ...
+});
+
+client.login(config.token)
